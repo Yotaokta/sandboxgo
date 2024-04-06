37fc827af8e0076c62735b21804b5d867d019481
Muhammad Yota <muhammadyota86@gmail.com>
2024-04-06 23:42:55 +0700

    adding price function

--- customer.go
+++ customer.go
@@ -9,5 +9,10 @@ func Checkout(name string, number_of_room int) string {
 }
 
 func order(nama string, number_of_room int) string {
-	return nama + " order " + strconv.Itoa(number_of_room) + " room"
+	return nama + " order " + strconv.Itoa(number_of_room) + " room with price " + strconv.Itoa(price(number_of_room))
+}
+
+func price(number_of_room int) int {
+	price := number_of_room * 350000
+	return price
 }
